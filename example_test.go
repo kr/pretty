cfd0d4de1e6142603b208d8bfce0d0660954f0a5
Alfonso Acosta <alfonso@stellar.org>
2020-08-27 21:55:54 +0200

    Implement omision of zeroed-fields with + flag

--- example_test.go
+++ example_test.go
@@ -10,11 +10,19 @@ func Example() {
 		a, b int
 	}
 	var x = []myType{{1, 2}, {3, 4}, {5, 6}}
-	fmt.Printf("%# v", pretty.Formatter(x))
+	fmt.Printf("%# v\n", pretty.Formatter(x))
+
+	var zeroedFields = []myType{{33, 0}, {a: 0, b: 34}}
+	// Note the '+' in the format
+	fmt.Printf("%# +v", pretty.Formatter(zeroedFields))
 	// output:
 	// []pretty_test.myType{
 	//     {a:1, b:2},
 	//     {a:3, b:4},
 	//     {a:5, b:6},
+	// }
+	// []pretty_test.myType{
+	//     {a:33},
+	//     {b:34},
 	// }
 }
